ea5134d9ac077b9724c25b7bc87d03bcd1f00fd3
27chandler <chandler27s@yahoo.com>
2021-08-07 20:39:16 +0100

    Created the start of the tile data grabber

--- Systemic-Crafting/Assets/GameData/Resources/BasicResources/ResourceBase.cs
+++ Systemic-Crafting/Assets/GameData/Resources/BasicResources/ResourceBase.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Resource", menuName = "Resource/Create new resource")]
 public class ResourceBase : ScriptableObject
 {
-    [SerializeField] string name;
+    [SerializeField] private string name;
 
-    // Property values
-    [SerializeField] float hardness;
-    [SerializeField] float flammability;
-    [SerializeField] float durability;
-    [SerializeField] float conductivity;
+    // Resource properties
+    [SerializeField] private float hardness;
+    [SerializeField] private float flammability;
+    [SerializeField] private float durability;
+    [SerializeField] private float conductivity;
+
+    //
+    public string Name { get { return name; } }
+    public float Hardness { get { return hardness; } }
+    public float Flammability { get { return flammability; } }
+    public float Durability { get { return durability; } }
+    public float Conductivity { get { return conductivity; } }
 }
--- Systemic-Crafting/Assets/Scripts/Managers/ResourceManager.cs
+++ Systemic-Crafting/Assets/Scripts/Managers/ResourceManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using TMPro;
 
 public class ResourceManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private Tilemap environment;
     [SerializeField] private List<ResourceBase> loadedResources = new List<ResourceBase>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +25,19 @@ public class ResourceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int grid_position = environment.WorldToCell(mouse_pos);
+
+            text.text = environment.GetTile(grid_position).name;
+
+            CreateNewResource();
+        }
+    }
+
+    void CreateNewResource()
+    {
+        ResourceBase resource = (ResourceBase)ScriptableObject.CreateInstance(typeof(ResourceBase));
     }
 }
