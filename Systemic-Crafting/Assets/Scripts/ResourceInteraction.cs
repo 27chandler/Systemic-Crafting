7db66f8397e1a0df8a4e2877bd8ebc62e1212af3
27chandler <chandler27s@yahoo.com>
2021-09-09 21:34:30 +0100

    Neatened the resource interaction class

--- Systemic-Crafting/Assets/Scripts/ResourceInteraction.cs
+++ Systemic-Crafting/Assets/Scripts/ResourceInteraction.cs
@@ -17,67 +17,70 @@ public class ResourceInteraction : MonoBehaviour
 
     public float TotalTimer { get { return totalTimer; } }
     public float Timer { get { return timer; } }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        // Mine Resource
         if (Input.GetMouseButton(0))
         {
-            if (minedResource == null)
-            {
-                Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                resourcePosition = environment.WorldToCell(mouse_pos);
-
-                minedResource = resourceManager.SearchResources(environment.GetTile(resourcePosition));
-
-                if (minedResource != null)
-                {
-                    totalTimer = minedResource.Hardness * miningSpeed;
-                    timer = totalTimer;
-                }
-            }
-            else
-            {
-                timer -= Time.deltaTime;
-
-                Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector3Int rounded_mouse_pos = new Vector3Int();
-                rounded_mouse_pos.x = Mathf.FloorToInt(mouse_pos.x);
-                rounded_mouse_pos.y = Mathf.FloorToInt(mouse_pos.y);
-                rounded_mouse_pos.z = 0;
-                if (rounded_mouse_pos != resourcePosition)
-                {
-                    minedResource = null;
-                }
-
-                if (timer <= 0.0f)
-                {
-                    timer = 0.0f;
-                    inv.AddQuantity(minedResource.Name, 1.0f);
-                    environment.SetTile(resourcePosition, null);
-                    minedResource = null;
-                }
-            }
+            MineResource();
         }
         else if (Input.GetMouseButton(1))
+        {
+            PlaceResource();
+        }
+    }
+
+    private void PlaceResource()
+    {
+        Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        resourcePosition = environment.WorldToCell(mouse_pos);
+
+        if ((environment.GetTile(resourcePosition) == null)
+            && (inv.FindQuantity(primarySlot.ResourceName) >= 1.0f))
+        {
+            TileBase placed_tile = resourceManager.SearchResources(primarySlot.ResourceName).Tile;
+
+            inv.AddQuantity(primarySlot.ResourceName, -1.0f);
+            environment.SetTile(resourcePosition, placed_tile);
+            resourcePosition = new Vector3Int();
+        }
+    }
+
+    private void MineResource()
+    {
+        if (minedResource == null)
         {
             Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             resourcePosition = environment.WorldToCell(mouse_pos);
 
-            if ((environment.GetTile(resourcePosition) == null)
-                && (inv.FindQuantity(primarySlot.ResourceName) >= 1.0f))
-            {
-                TileBase placed_tile = resourceManager.SearchResources(primarySlot.ResourceName).Tile;
+            minedResource = resourceManager.SearchResources(environment.GetTile(resourcePosition));
 
-                inv.AddQuantity(primarySlot.ResourceName, -1.0f);
-                environment.SetTile(resourcePosition, placed_tile);
-                resourcePosition = new Vector3Int();
+            if (minedResource != null)
+            {
+                totalTimer = minedResource.Hardness * miningSpeed;
+                timer = totalTimer;
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+
+            Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int rounded_mouse_pos = new Vector3Int();
+            rounded_mouse_pos.x = Mathf.FloorToInt(mouse_pos.x);
+            rounded_mouse_pos.y = Mathf.FloorToInt(mouse_pos.y);
+            rounded_mouse_pos.z = 0;
+            if (rounded_mouse_pos != resourcePosition)
+            {
+                minedResource = null;
+            }
+
+            if (timer <= 0.0f)
+            {
+                timer = 0.0f;
+                inv.AddQuantity(minedResource.Name, 1.0f);
+                environment.SetTile(resourcePosition, null);
+                minedResource = null;
             }
         }
     }
